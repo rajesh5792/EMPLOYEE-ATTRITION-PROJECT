3f258f1e167f6286a31c099328749a314dea5e15
Rajesh kannan <rajeshkannan26473@gmail.com>
2026-05-06 16:37:40 +0530

    Switched to Logistic Regression model and some changes in logics

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
+    lr = pickle.load(f)
 
 # Page config
 st.set_page_config(page_title="Attrition Predictor", layout="centered")
@@ -13,29 +13,56 @@ st.write("Predict whether an employee is likely to leave the company.")
 
 st.divider()
 
-# Input section
 st.subheader("Enter Employee Details")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    age = st.slider("Age", 18, 60)
-    years = st.slider("Years at Company", 0, 40)
+    age = st.slider("Age", 18, 60, 30)
+
+    years = st.slider("Years at Company", 0, 40, 5)
+
+    job_satisfaction = st.slider("Job Satisfaction", 1, 4, 2)
 
 with col2:
-    income = st.number_input("Monthly Income", min_value=1000, max_value=20000)
+    income = st.number_input(
+        "Monthly Income",
+        min_value=1000,
+        max_value=50000,
+        value=10000
+    )
+
+    work_life = st.slider("Work Life Balance", 1, 4, 2)
+
+    overtime = st.selectbox("OverTime", ["Yes", "No"])
 
 st.divider()
 
 # Predict button
 if st.button("Predict Attrition"):
-    input_data = np.array([[age, income, years]])
-    proba = model.predict_proba(input_data)[0][1]
-    st.subheader("Prediction Result")
 
-    if proba > 0.4:
-        st.error("🔴 High Risk")
-    elif proba > 0.25:
-        st.warning("🟠 Medium Risk")
+    overtime_yes = 1 if overtime == "Yes" else 0
+
+    import pandas as pd
+
+    input_data = pd.DataFrame([{
+        'Age': age,
+        'MonthlyIncome': income,
+        'YearsAtCompany': years,
+        'JobSatisfaction': job_satisfaction,
+        'WorkLifeBalance': work_life,
+        'OverTime_Yes': overtime_yes
+    }])
+
+    proba = lr.predict_proba(input_data)[0][1]
+
+    st.subheader("Prediction Result")
+    st.write(f"Attrition Probability: {proba:.2%}")
+    if proba > 0.20:
+        st.error(f"High Attrition Risk ({proba:.2%})")
+
+    elif proba > 0.10:
+        st.warning(f"Medium Attrition Risk ({proba:.2%})")
+
     else:
-        st.success("🟢 Low Risk")
+        st.success(f"Low Attrition Risk ({proba:.2%})")
